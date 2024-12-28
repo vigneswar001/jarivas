590dead0f05bf5734edab4e257eebbc1ac5de707
vigneswar001 <eng23am0123@dsu.edu.in>
2024-12-28 19:03:26 +0530

    Create calculator.java
    simple calculator

---
+++ calculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+        
+        System.out.print("Choose operation (+, -, *, /): ");
+        char operation = scanner.next().charAt(0);
+        
+        double result;
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Cannot divide by zero.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operation.");
+                return;
+        }
+        
+        System.out.println("Result: " + result);
+        scanner.close();
+    }
+}
